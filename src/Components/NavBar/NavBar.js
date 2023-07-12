a4c81216e12798bfd2bfae9853c63a20fe131674
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-12 00:30:49 -0700

    NavBar Done

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
 
 export default function NavBar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="container-fluid">
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container">
+                        <a className="navbar-brand" href="#">
+                            <img src="./images/logo.png" style={{ width: '50px' }} alt="" />
+                            <span className="fw-bold m-2">TROPIKO</span>
+                        </a>
+                        <button
+                            className="navbar-toggler collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="navbar-collapse collapse show" id="navbarSupportedContent" style={{}}>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active me-lg-5" aria-current="page" href="https://www.facebook.com/profile.php?id=100085130599968" target="_blank">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active me-lg-5" href="https://www.fiverr.com/users/editorsspot1" target="_blank">Fruits</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active me-lg-5" href="#Services">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active me-lg-5" href="https://www.fiverr.com/users/editorsspot1" target="_blank">Contact us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active me-lg-5" href="https://www.facebook.com/profile.php?id=100085130599968" target="_blank">
+                                        <img src="./images/search-icon.png" alt="Search" />
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <a href="https://www.facebook.com/profile.php?id=100085130599968" target="_blank">
+                                <button type="button" className="btn btn-primary" style={{ backgroundColor: 'rgba(255, 68, 0, 0.767)' }}>
+                                    Get A Quote
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import NavBar from '../Components/NavBar/NavBar'
 
 export default function HomePage() {
   return (
     <div>
+        
+        <NavBar />
       
-      
+
     </div>
   )
 }
