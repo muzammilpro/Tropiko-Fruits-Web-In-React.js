2dc0c88a855b8a883fbcd29c050ad314c826bee2
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-12 01:39:42 -0700

    DO Some Changes

---
+++ src/Components/FreshFruit/FreshFruit.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function FreshFruit() {
+  return (
+    <div>
+      <div>
+            <center>
+                <h1>Fresh Fruits</h1><br />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus quia vero nobi</p>
+            </center> <br />
+
+
+            <div className="cont5">
+                <div>
+                    <h3>Best Fresh Oeange</h3> <br /><br />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quaerat repellat eius id velit
+                        doloribus
+                        odio ea, nam eligendi dicta necessitatibus aliquid soluta <br /> laudantium fugiat similique
+                        assumenda
+                        deserunt cum vitae. Ratione?</p>
+                    <button type="button" className="btn btn-dark">Buy Now</button>
+                </div>
+                <div><img src="./images/orange.png" alt="img" /></div>
+            </div>
+
+            
+           
+        </div>
+        
+    </div>
+  )
+}
