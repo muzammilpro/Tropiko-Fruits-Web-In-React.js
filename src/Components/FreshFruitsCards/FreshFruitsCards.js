1f5837fcac6e86b445ecefed4cf967c27772dae1
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-13 10:47:55 -0700

    DO Some Changes

--- src/Components/FreshFruitsCards/FreshFruitsCards.js
+++ src/Components/FreshFruitsCards/FreshFruitsCards.js
@@ -6,7 +6,7 @@ export default function FreshFruitsCards() {
     <div>
       <div className="cont5">
                 <div>
-                    <h3>Best Fresh Oeange</h3> <br /><br />
+                    <h3>Best Fresh Orange</h3> <br /><br />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quaerat repellat eius id velit
                         doloribus
                         odio ea, nam eligendi dicta necessitatibus aliquid soluta <br /> laudantium fugiat similique
