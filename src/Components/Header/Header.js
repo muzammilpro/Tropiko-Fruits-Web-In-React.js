579db3b3e8a0c4938d5e16e3784c5200a1234809
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-13 10:11:40 -0700

    DO Some Changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,4 @@
 import Img1 from '../../Images/slide-img.png'
-import Button from '../Buttons/Buttons'
 
 export default function Header() {
     return (
@@ -9,27 +8,28 @@ export default function Header() {
                 <div className="container1">
                     <div className="container2">
                         <h1>Welcome to <br /> our Fruit Shop</h1><br />
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, est ad dolorem aliquam nostrum
-                                eligendi
-                                molestias placeat aspernatur sed. Harum repellendus quia tempora officiis voluptas? Veniam
-                                expedita
-                                eaque ea molestiae?</p>
-                            <br />
-                                <div>
-                                <span>
-                                 
-                                <Button />
-                                </span>
-                                
-                                </div>
-                                
-                            </div>
-                            <div className="container3">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, est ad dolorem aliquam nostrum
+                            eligendi
+                            molestias placeat aspernatur sed. Harum repellendus quia tempora officiis voluptas? Veniam
+                            expedita
+                            eaque ea molestiae?</p>
+                        <br />
+                        <div>
 
-                                <img src={Img1} alt="img" width="60%" height="50%" />
 
-                            </div>
+                            <button type="button" id="btn1" className="btn btn-primary">Shop Now</button>
+                            <button type="button" className="btn btn-dark">Contact Us</button>
+
+
+                        </div>
+
                     </div>
+                    <div className="container3">
+
+                        <img src={Img1} alt="img" width="60%" height="50%" />
+
+                    </div>
+                </div>
             </center>
 
         </div>
